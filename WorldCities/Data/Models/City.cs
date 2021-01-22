1f2783c35f7980ba67ca252ef08411bdac8ce6ee
Denis <panovds72@gmail.com>
2021-01-22 14:10:24 +0500

    Создал классы City и Country

---
+++ WorldCities/Data/Models/City.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorldCities.Data.Models
+{
+    public class City
+    {
+        public City()
+        {
+
+        }
+        /// <summary>
+        /// Уникальный идентификатор и первичный ключ для этого города.
+        /// </summary>
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Название города (в формате UTF8).
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Название города (в формате ASCII).
+        /// </summary>
+        public string Name_ASCII { get; set; }
+
+        /// <summary>
+        /// Широта города.
+        /// </summary>
+        public decimal Lat { get; set; }
+
+        /// <summary>
+        /// Долгота города.
+        /// </summary>
+        public decimal Lon { get; set; }
+
+        /// <summary>
+        /// Идентификатор страны (внешний ключ).
+        /// </summary>
+        [ForeignKey("Country")]
+        public int CountryId { get; set; }
+
+        /// <summary>
+        /// Страна относящаяся к этому городу.
+        /// </summary>
+        public virtual Country Country { get; set; }
+    }
+}
---
+++ WorldCities/Data/Models/Country.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorldCities.Data.Models
+{
+    public class Country
+    {
+        public Country()
+        {
+
+        }
+
+        /// <summary>
+        /// Уникальный идентификатор и первичный ключ страны.
+        /// </summary>
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Название страны (в формате UTF8).
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Код страны (в формате ISO 3166-1 ALPHA-2).
+        /// </summary>
+        public string ISO2 { get; set; }
+
+        /// <summary>
+        /// Код страны (в формате ISO 3166-1 ALPHA-3).
+        /// </summary>
+        public string ISO3 { get; set; }
+
+        /// <summary>
+        /// Список, содержащий все города, относящиеся к этой стране.
+        /// </summary>
+        public virtual List<City> Cities { get; set; }
+    }
+}
